93ae4473b3a4355799613e23c83b2888f76b46a7
FK78 <34196381+FK78@users.noreply.github.com>
2025-08-15 12:45:28 +0100

    finish CH1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
 def main():
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -13,16 +13,20 @@ def main():
 
     if len(sys.argv) > 1:
         prompt = sys.argv[1]
+        messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
         response = client.models.generate_content(
-            model="gemini-2.0-flash-001", contents=prompt
+            model="gemini-2.0-flash-001", contents=messages
         )
 
         print(response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if "--verbose" in sys.argv:
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
         print("Please provide a prompt as a command line argument.")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
