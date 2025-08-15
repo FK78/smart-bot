f326bb84b49343e567f21157742d17a59649a513
FK78 <34196381+FK78@users.noreply.github.com>
2025-08-15 12:33:00 +0100

    require user input for usage

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from smart-bot!")
+import os
+import sys
+from google import genai
+from dotenv import load_dotenv
 
+
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", contents=prompt
+        )
+
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("Please provide a prompt as a command line argument.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
